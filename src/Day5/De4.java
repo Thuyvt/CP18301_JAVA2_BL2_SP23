8c2aa74eb5a9efcfb6180a4f49bf21b897db27ac
thuyvt <thuyvt1005@gmail.com>
2023-03-16 11:14:23 +0700

    day5

--- src/Day5/De4.java
+++ src/Day5/De4.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Day4;
+package Day5;
 
 import java.util.ArrayList;
 import java.util.List;
